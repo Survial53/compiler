60a75ada64b4e269c38038be112bbe762ce9cab2
Survial53 <negatorhangkok@gmail.com>
2015-10-26 16:54:31 +0300

    my_lexical_analyzer

---
+++ task1.cpp
@@ -0,0 +1,18 @@
+#include <lexer.h>
+
+int main()
+{
+	CLexer Lexer;
+	Lexer.Init();
+	ifstream inputFile;
+	inputFile.open("prog.txt");
+	if (!inputFile.is_open()) {
+		cout << "Error opening file" << endl;
+		return 0;
+	}
+	Lexer.Lex(inputFile);
+	Lexer.SaveTokens(cout);
+	inputFile.close();
+	return 0;
+}
+
